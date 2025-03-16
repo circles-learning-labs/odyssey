3717e8c26b8dfb2b24b84a43b6db822146fcae23
Bernard Duggan <bernard@circl.es>
2025-03-17 10:40:52 +1100

    Initial release

--- mix.exs
+++ mix.exs
@@ -1,9 +1,12 @@
 defmodule Odyssey.MixProject do
   use Mix.Project
 
+  @github_url "https://github.com/circles-learning-labs/odyssey"
+
   def project do
     [
       app: :odyssey,
+      description: "A workflow engine for Elixir",
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
@@ -16,7 +19,9 @@ defmodule Odyssey.MixProject do
         "coveralls.post": :test,
         "coveralls.html": :test,
         "coveralls.cobertura": :test
-      ]
+      ],
+      source_url: @github_url,
+      package: package()
     ]
   end
 
@@ -27,12 +32,25 @@ defmodule Odyssey.MixProject do
     ]
   end
 
+  def package do
+    [
+      name: "odyssey",
+      licenses: ["MIT"],
+      maintainers: ["Bernard Duggan"],
+      links: %{
+        "GitHub" => @github_url
+      },
+      exclude_patterns: [~r/.*~/, ~r/src\/.*\.erl/]
+    ]
+  end
+
   defp deps do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ecto_sql, "~> 3.0"},
       {:eventually, "~> 1.1", only: :test, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.18", only: :test, runtime: false},
       {:igniter, "~> 0.5", only: [:dev], runtime: false},
       {:oban, "~> 2.19"},
