cda0638195ae97ab5b2ec82e46b9919d4ae69844
Bernard Duggan <bernard@circl.es>
2025-03-25 15:21:49 +1100

    Bump version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Odyssey.MixProject do
     [
       app: :odyssey,
       description: "A workflow engine for Elixir",
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
